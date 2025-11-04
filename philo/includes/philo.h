9c78fe169297df624f150feccde6f26bb076262d
Talen Lavareda Pinto <TalenGitHub0@gmail.com>
2025-11-04 20:20:32 -0300

    Creating initial sources to mandatory

---
+++ philo/includes/philo.h
@@ -0,0 +1,14 @@
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h>
+# include <string.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <sys/time.h>
+# include <pthread.h>
+
+# define SUCESS 0
+# define FAIL 1
+
+#endif
