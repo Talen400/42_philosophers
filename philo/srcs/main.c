d0bb9c1ff42339572e7b4eb2bcb37e949043c205
Talen Lavareda Pinto <TalenGitHub0@gmail.com>
2025-11-05 00:48:35 -0300

    understand mutex

--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -6,54 +6,62 @@
 /*   By: tlavared <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/04 01:19:09 by tlavared          #+#    #+#             */
-/*   Updated: 2025/11/04 21:05:33 by tlavared         ###   ########.fr       */
+/*   Updated: 2025/11/05 00:14:52 by tlavared         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
 
+typedef struct s_data
+{
+	long int			num;
+	pthread_mutex_t	lock;
+}	t_data;
+
 void	*ft_wait(void *arg)
 {
-	int	num;
-	int	result;
+	t_data	*shared_data;
 
-	num = *(int *) arg;
-	result = 1;
-	while (num)
+	shared_data = (t_data *) arg;
+	pthread_mutex_lock(&shared_data->lock);
+	while (shared_data->num < 100)
 	{
-		result *= num;
-		num--;
+		shared_data->num++;
+		printf("%ld\n", shared_data->num);
 	}
-	printf("Done: %d\n", result);
+	printf("Done: %ld\n", shared_data->num);
+	pthread_mutex_unlock(&shared_data->lock);
 	(void ) arg;
 	return (NULL);
 }
 
 int	main(int argc, char **argv)
 {
-	pthread_t	thread;
-	pthread_t	thread2;
+	pthread_t	t1;
+	pthread_t	t2;
 	int			err;
-	int			num1;
-	int			num2;
+	t_data		data;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (FAIL);
 	}
-	num1 = ft_atoi(argv[1]);
-	err = pthread_create(&thread, NULL, ft_wait, &num1);
-	num2 = num1 - 1;
-	err += pthread_create(&thread2, NULL, ft_wait, &num2);
+	if (pthread_mutex_init(&data.lock, NULL) != 0)
+	{
+		printf("Mudex init failed. \n");
+	}
+	data.num= 0;
+	err = pthread_create(&t1, NULL, ft_wait, &data);
+	err += pthread_create(&t2, NULL, ft_wait, &data);
 	if (err != 0)
 	{
 		printf("An error ocurred: %d\n", err);
 		return (FAIL);
 	}
 	printf("Waiting for thread to end...\n");
-	pthread_join(thread, NULL);
-	pthread_join(thread2, NULL);
+	pthread_join(t1, NULL);
+	pthread_join(t2, NULL);
 	printf("Thread ended.\n");
 	(void ) argc;
 	(void ) argv;
